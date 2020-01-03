3aa9c16ab74e6b12011c4ba056bc8427c0589fc8
phamdinhhung212 <59237532+phamdinhhung212@users.noreply.github.com>
2020-01-04 06:07:02 +0700

    Update main-js.js

--- js/main-js.js
+++ js/main-js.js
@@ -1,12 +1,13 @@
 // header
-// xử lí ẩn hiện respon left và respon right
+// handle hide and show respon left and respon right 
 $(document).ready(function() {
 	$("#header span#icon-respon-menu").click(function() {
+		$("#header #respon-right").stop().fadeToggle(500);
 		$("#header #respon-right .pt").stop().slideToggle(500);
 		$("#site").removeClass('open-respon-left');
 		$("#icon-tabbar").removeClass('fa-chevron-circle-left').addClass('fa-chevron-circle-right');
 	});
-	// xử lí chỉ hiện respon right khi màn hình < 667px 
+	// handle show respon right when screen less 667px
 	$(window).resize(function() {
 		if( $(window).width() > 650 ){
 			$("#header #respon-right .pt").slideUp(500);
@@ -15,7 +16,7 @@ $(document).ready(function() {
 });
 
 
-// xử lý hiện thanh tìm kiếm
+// handle show and hide search bar
 
 
 // open respon left
@@ -25,14 +26,19 @@ $(document).ready(function() {
 		$("#icon-tabbar").toggleClass("fa-chevron-circle-right fa-chevron-circle-left");
 		$("#header #respon-right .pt").slideUp(500);
 	});
-	// content click cả respon right và respon left đều ẩn
+	// handle content click both respon right and respon left hide 
 	$("#content").click(function() {
 		$("#site").removeClass('open-respon-left');
 		$("#icon-tabbar").removeClass('fa-chevron-circle-left').addClass('fa-chevron-circle-right');
 		$("#header #respon-right .pt").slideUp(500);
 	});
+	// when resize then respon left automatic close
+	$(window).resize(function() {
+		$("#site").removeClass('open-respon-left');
+		$("#icon-tabbar").removeClass('fa-chevron-circle-left').addClass('fa-chevron-circle-right');
+	});
 });	
-// xử lí ẩn hiện menu respon
+// handle hide and show menu child in menu respon right when clicked
 $(document).ready(function() {
 	$("#header span#icon-menu").click(function() {
 		$(this).toggleClass("move"); 
@@ -41,37 +47,62 @@ $(document).ready(function() {
 	})
 });
 
-// xử lí ẩn hiện search fa-bars
+// handle hide and show search bar
 $(document).ready(function() {
-	$(".list-Utilities a.search , #header .icon-search, #header #search-bar #search-box .close-s").click(function() {
+	// click search in sideabr
+	$(".list-Utilities span.search.s").click(function() {
 		$("#header #search-bar").stop().fadeToggle(500);
+		$("#content").toggleClass('opacity-ct');
+	});
+	// click search in respon-left
+	$(".list-Utilities span.search.h").click(function() {
+		$("#header #search-bar").stop().fadeToggle(500);
+		$("#content").toggleClass('opacity-ct');
 		$("#site").removeClass('open-respon-left');
+		$("#icon-tabbar").toggleClass('fa-chevron-circle-left fa-chevron-circle-right')
+	});
+	// click search on header
+	$("#header .icon-search").click(function() {
+		$("#header #search-bar").stop().fadeToggle(500);
+		$("#content").toggleClass('opacity-ct');
+		$("#site").removeClass('open-respon-left');
+		if( $("#content").is('.opacity-ct') ) {
+			$("#icon-tabbar").removeClass('fa-chevron-circle-left').addClass('fa-chevron-circle-right')
+		}
+	});
+	// click close search
+	$("#header #search-bar #search-box .close-s").click(function() {
+		$("#header #search-bar").stop().fadeToggle(500);
+		$("#content").toggleClass('opacity-ct');
 	});
 });
-
-
-// content
-// content -> widget.product -> widget content hide-2 ( xử lý ẩn hiện hình ảnh con )
+// -------------------- content -----------------
+// -------------------------------------
+// handle hide and show img child
 $(document).ready(function() {
 	$("ul.list-product li").hover( function() {
 		// hover in 
 		$(this).addClass('open-list-thumb');
 		$(this).children('.more-product').addClass('open-list-thumb');
-		$(this).children('.add-basket').stop().fadeIn(1000);
-		// đếm số ảnh con
+		$(this).children('.love-this').stop().fadeIn(1000);
+		// count img child
 		var cout = 0;
 		$(this).children('.more-product').children('.hide-2').children('ul.list-thumb').children('li').each(function() {
 			cout ++ ;
 		});
+		// handle when num img child = 3
 		if( cout == 3 ) {
 			$(this).children('.more-product').children('.hide-2').children('ul.list-thumb').addClass('three-img');
 		}
+		// handle when num img child = 2
 		if( cout == 2 ) {
 			$(this).children('.more-product').children('.hide-2').children('ul.list-thumb').addClass('two-img');
 		}
+		// handle when num img child = 1
 		if( cout == 1 ) {
 			$(this).children('.more-product').children('.hide-2').children('ul.list-thumb').addClass('one-img');
 		}
+		// handle when num img child = 0
 		if( cout == 0 ) {
 			$(this).removeClass('open-list-thumb');
 			$(this).children('.more-product').removeClass('open-list-thumb');
@@ -80,21 +111,77 @@ $(document).ready(function() {
 		// hover out
 		$(this).removeClass('open-list-thumb');
 		$(this).children('.more-product').removeClass('open-list-thumb');
-		$(this).children('.add-basket').stop().fadeOut(500);
+		$(this).children('.love-this').stop().fadeOut(500);
 	});
-	// vô hiệu hóa thẻ a
+	// disable <a>
 	$("ul.list-product li a").click(function() {
 		return false;
 	});
 })
 
-
-
-// footer
-// xử lý khi submit nhưng phần chức năng đó chưa được cập nhật
+// open scroll bar and scroll top
 $(document).ready(function() {
-	$("#signUp-f , #f-sp").submit(function() {
+	$(window).scroll(function() {
+		if( $(window).scrollTop() > 300 ) {
+			$("#back-top").addClass('open-scroll');
+		}
+		else {
+			$("#back-top").removeClass('open-scroll');
+		}
+	});
+	$("#back-top span.icon-scroll").click(function() {
+		$("html,body").animate({
+			scrollTop : 0,
+		},1000);
+	});
+});
+
+
+// click show img 
+$(document).ready(function() {
+	$(".more-product .hide-2 ul.list-thumb li").hover(function() {
+		var get_src_img = $(this).children('img').attr('src');
+		$(".more-product .hide-2 ul.list-thumb li").removeClass('active-li');
+		$(this).addClass('active-li');
+		$(this).parents('li').children('a').children('img').attr('src',get_src_img)
+	},function() {
+
+	});
+});
+// function like product
+$(document).ready(function() {
+	$("ul.list-product li span.love-this").click(function() {
+		$(this).parent('li').children('i').stop().fadeToggle();	
+	});
+	$("ul.list-product li i").click(function() {
+		$(this).fadeToggle();
+	});
+});
+// ----------------- end content ---------------------
+
+
+
+// -------------------- footer --------------------
+// handle when submit but part function not update
+$(document).ready(function() {
+	$("#signUp-f , #f-sp, #search-box").submit(function() {
 		alert("Phạm Đình Hùng xin lỗi, chức năng này chưa được cập nhật");
 		return false;
+	});
+});
+// ------------------- end footer ------------------
+
+
+
+
+
+// handle auto self moving
+$(document).ready(function() {
+	var positionActiveCurrent = $(".active-bn").index();
+	$(".btn-next-banner").click(function() {
+		console.log(positionActiveCurrent);
+		var x = positionActiveCurrent + 1;
+		$(".slide-box .list-slide .slide").eq(positionActiveCurrent + 1).stop().fadeIn();
+			positionActiveCurrent ++;
 	});
 });
